158c2578261214787d3f93d21be05f115ba37c1e
vlad <irenfri@gmail.com>
2021-12-24 18:37:36 +0300

    Length of Tuple

--- challenges/easy/tuple-length.ts
+++ challenges/easy/tuple-length.ts
@@ -23,7 +23,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Length<T extends readonly unknown[]> = T['length'];
+type Length<T extends readonly unknown[]> = T['length']
 
 
 /* _____________ Test Cases _____________ */
