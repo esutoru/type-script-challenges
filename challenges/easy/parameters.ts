7c452e85cec6cda350cb1b9b85577a68915bf495
vlad <irenfri@gmail.com>
2022-01-08 20:59:32 +0300

    Parameters

---
+++ challenges/easy/parameters.ts
@@ -0,0 +1,30 @@
+/*
+  3312 - Parameters
+  -------
+  by midorizemi (@midorizemi) #easy #infer #tuple #built-in
+
+  ### Question
+
+  Implement the built-in Parameters<T> generic without using it.
+
+  > View on GitHub: https://tsch.js.org/3312
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type MyParameters<T> = T extends (...args: infer Params) => unknown ? Params : never
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+const foo = (arg1: string, arg2: number): void => {}
+const bar = (arg1: boolean, arg2: {a: 'A'}): void => {}
+const baz = (): void => {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, {a: 'A'}]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
