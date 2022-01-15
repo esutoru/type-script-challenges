d90da62d058b1eca53816666708338ea33e22f92
vlad <irenfri@gmail.com>
2022-01-15 21:18:02 +0300

    Permutation

---
+++ challenges/medium/permutation.ts
@@ -0,0 +1,38 @@
+/*
+  296 - Permutation
+  -------
+  by Naoto Ikuno (@pandanoir) #medium #union
+
+  ### Question
+
+  Implement permutation type that transforms union types into the array that includes permutations of unions.
+
+  ```typescript
+  type perm = Permutation<'A' | 'B' | 'C'>; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+  ```
+
+  > View on GitHub: https://tsch.js.org/296
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+// See issue: https://github.com/microsoft/TypeScript/issues/31751
+type isNever<T> = [T] extends [never] ? true : false
+
+// See explanation: https://github.com/type-challenges/type-challenges/issues/614
+type Permutation<T, C = T> = isNever<T> extends true
+  ? []
+  : C extends C
+    ? [C, ...Permutation<Exclude<T, C>>]
+    : []
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
