e9d0de0d9bbd09074397ba0dd8b72bdad44c72a8
vlad <irenfri@gmail.com>
2022-01-16 16:50:38 +0300

    Kebabcase

---
+++ challenges/medium/kebabcase.ts
@@ -0,0 +1,34 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+
+  ### Question
+
+  `FooBarBaz` -> `foo-bar-baz`
+
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type KebabCase<Str> = Str extends `${infer First}${infer Rest}`
+  ? Rest extends Uncapitalize<Rest>
+    ? `${Uncapitalize<First>}${KebabCase<Rest>}`
+    : `${Uncapitalize<First>}-${KebabCase<Rest>}`
+  : Str;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+]
