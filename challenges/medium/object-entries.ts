13a40fde303c805d12a2360a935f852204b96a5e
vlad <irenfri@gmail.com>
2022-01-20 21:28:10 +0300

    Object Entries

---
+++ challenges/medium/object-entries.ts
@@ -0,0 +1,48 @@
+/*
+  2946 - ObjectEntries
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+
+  ### Question
+
+  Implement the type version of ```Object.entries```
+
+  For example
+
+  ```typescript
+  interface Model {
+    name: string;
+    age: number;
+    locations: string[] | null;
+  }
+  type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
+  ```
+
+  > View on GitHub: https://tsch.js.org/2946
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type ObjectEntries<T, K = keyof T> = K extends keyof T ? [K, Required<T>[K]] : never;
+
+type t = ObjectEntries<Model>
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null];
+
+
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>,ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>,ModelEntries>>,
+]
